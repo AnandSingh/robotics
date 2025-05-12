c2c1f4bfcbbfa6fc1d41b268d3e36effd661d058
Anand Singh <asingh.sqm@gmail.com>
2025-05-11 18:02:41 -0700

    the robot is doing the 3dmovie.py

--- run_3dmovie.py
+++ run_3dmovie.py
@@ -3,14 +3,19 @@ from NexusAttachement import *
 
 
 def run_mission(robo, arm):
-    # robo.straight_drive(100)
+
     print("test")
-    arm.move_left(300)
-    arm.move_right(300)
+    robo.straight_drive(-320)   
+    arm.move_left(300)  
+    #arm.move_left_time(20)
+    robo.straight_drive(350)
+    arm.move_left(-350)      
+    #arm.move_left_time(20)
+
+    
+    
+    # arm.move_right(-300)
     # arm.move_right_time(300)
-
-    # arm.move_left_time(40)
-
     # robo.pivot_turn(-76)
     # robo.straight_drive(940)
     # robo.straight_drive(-940)
