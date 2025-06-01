39c23219ed5065ba90dfd67ace590e18c296a22e
Faris Mudassir <farismudassir3@gmail.com>
2025-05-31 18:44:41 -0700

    making loading script better

--- .vscode/runRobot.sh
+++ .vscode/runRobot.sh
@@ -1,16 +1,32 @@
 #!/bin/bash 
 
-#source ../robotName.sh
-echo ">> Executing: $0"
-echo ">> RobotName: $1"
-echo ">> File: $2"
-source $1
-echo "Robot Name is ? $robotName"
 
-filename=$(basename "$2")
-if [[ "${filename##*.}" != "py" ]]; then
-  echo "Not a Python file, so exiting !!!"
+# Color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color (reset)
+
+echo ">> Executing      : $0"
+echo ">> RobotName File : $1"
+echo ">> Python File    : $2"
+
+robotName=$(cat $1)
+export ROBOT_NAME=$robotName
+
+# Check if the environment variable is set
+if [ -z "$ROBOT_NAME" ]; then
+  echo -e  "${RED} Your Robot Name is NOT set.${NC}"
+
 else
-    echo "Loading $2 on $robotName"
-    pybricksdev run ble --name $robotName $2
+  echo -e "${GREEN}Your Robot Name is ${YELLOW}$robotName${NC}"
+
+  filename=$(basename "$2")
+  if [[ "${filename##*.}" != "py" ]]; then
+    echo -e "${RED}Not a Python file, so exiting !!!${NC}"
+  else
+      echo -e "${GREEN}Loading $2 on  ${YELLOW}$ROBOT_NAME${NC}"
+      pybricksdev run ble --name $ROBOT_NAME $2
+      echo -e "${GREEN}------- DONE ---------${NC}"
+  fi
 fi
