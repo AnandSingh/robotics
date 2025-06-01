4b946a5783aa427bc9cb39eb91f4c4ff4a2f61fe
Anand Singh <anand.krs@gmail.com>
2025-05-31 18:26:32 -0700

    change python to a bash script for git pull

--- .vscode/gitpull.sh
+++ .vscode/gitpull.sh
@@ -1,3 +1,26 @@
-echo "Git pull in 5 seconds..."
-sleep 5
-git pull
+#!/bin/bash
+
+# Color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+
+NC='\033[0m' # No Color (reset)
+
+robotName=$(cat /config/workspace/MY_ROBOT_NAME)
+
+export ROBOT_NAME=$robotName
+
+
+# Check if the environment variable is set
+if [ -z "$ROBOT_NAME" ]; then
+  echo -e  "${RED} Your Robot Name is NOT set.${NC}"
+else
+  echo -e "${GREEN}Your Robot Name is '$ROBOT_NAME' !!${NC}"
+fi
+
+
+echo -e "${YELLOW}Doing git fetch and pull ...${NC}"
+#sleep 3
+git fetch && git pull
+echo -e "${YELLOW}------- DONE -------${NC}"
