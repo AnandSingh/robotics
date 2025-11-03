0fa3acc0ea62f97fab22be67941d0f5b6b9f552f
nexus warrior <nexuswarrior@robo.site>
2025-11-02 19:05:22 -0800

    ygsybyg

--- run_MineshaftExplorer_MapReveal.py
+++ run_MineshaftExplorer_MapReveal.py
@@ -6,4 +6,19 @@ def run_mission(robo, arm):
     # robo.straight_drive(100)
     print("test")
 
-    robo.drive(750)
+    robo.drive(-630)
+    robo.pivot_turn(-30)
+    arm.move_left(-50)
+    arm.move_right(-3)
+    robo.drive(-75)
+    robo.pivot_turn(-10)
+    robo.drive(17)
+    arm.move_right(50)
+    robo.drive(-30)
+    arm.move_right(-160)
+
+
+if __name__ == "__main__":
+    robo = NexusDrive()
+    arm = NexusAttachement()
+    run_mission(robo, arm)
